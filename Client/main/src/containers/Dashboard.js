6eadd282237b181bee051306cfd82466a0c57631
barak98 <barak98@gmail.com>
2021-05-12 16:00:23 +0300

    Update Dashboard.js

--- Client/main/src/containers/Dashboard.js
+++ Client/main/src/containers/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Navbar from "../components/Navbar";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 
 export default function DashBoard() {
 
