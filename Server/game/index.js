9a4dc4dd4f62f301438f0980c5b4e21b355b92fb
barak98 <barak98@gmail.com>
2021-05-24 13:28:38 +0300

    gameServer update

--- Server/game/index.js
+++ Server/game/index.js
@@ -10,6 +10,8 @@ const app = express()
 
 const server = app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}...`));
 
+let dic = new Dictionary()
+
 // socket server
 const socket = require('socket.io');
 const io = socket(server, {
@@ -21,6 +23,7 @@ const io = socket(server, {
 io.on('connection', (socket) => {
     socket.on('reqTurn', (data) => {
         const room = JSON.parse(data).room
+        console.log('in reqTurn')
         io.to(room).emit('playerTurn', data)
     })
 
@@ -29,8 +32,18 @@ io.on('connection', (socket) => {
     })
 
     socket.on('join', room => {
-        socket.join(room)
-        io.to(room).emit('opponent_joined')
+        if(!(dic.isKeyIn(room))){
+            socket.join(room);
+            dic.add(room, 1);
+        }
+        else{
+            if(dic.findAt(room) === 1){
+            socket.join(room)
+            dic.AddToValue(room);
+            io.to(room).emit('opponent_joined')
+            }
+        }
+
     })
 
     socket.on('reqRestart', (data) => {
@@ -38,3 +51,49 @@ io.on('connection', (socket) => {
         io.to(room).emit('restart')
     })
 });
+
+
+function Dictionary(){
+    this.dataStore =[];
+
+
+    this.add = function(key ,value){
+        if(key && value){
+            this.dataStore.push({
+                key: key,
+                value: value
+            })
+            return this.dataStore;
+        }
+    };
+
+    this.removeAt = function(key){
+        for (let i = 0; i<this.dataStore.length;i++){
+            if(this.dataStore[i].key === key)
+                this.dataStore.splice(this.dataStore[i],1)
+        }
+    };
+
+    this.isKeyIn = function(key){
+        for(let i = 0; i<this.dataStore.length;i++){
+            if(this.dataStore[i].key === key)
+            return true;
+        }
+        return false;
+    };
+
+    this.findAt = function(key){
+        for(let i = 0; i<this.dataStore.length;i++){
+            if (this.dataStore[i].key===key){
+                return this.dataStore[i].value
+            }
+        }
+    };
+
+    this.AddToValue = function(key){
+        this.dataStore.forEach(e => {
+            if(e.key === key)
+                e.value = 2;
+        });
+    };
+}
